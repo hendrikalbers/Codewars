5f220a2296eb5e1b02e1fe8ade4aed85ce7baa26
Hendrik Albers <hendrik.albers@rossmann.de>
2017-09-06 13:25:11 +0200

    Uebung JadenCase

---
+++ Uebungen/src/multiples/of/drei/und/fuenf/AufrufSolution.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiples.of.drei.und.fuenf;
+
+/**
+ *
+ * @author Hendrik.Albers
+ */
+public class AufrufSolution {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Solution s = new Solution();
+        
+        
+        System.out.println(s.solution(10));
+    }
+    
+}
